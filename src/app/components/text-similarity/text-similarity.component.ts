fb781a38c8108ff6058573c2b0fd8edbecead788
Maksa <milosmilos@ymail.com>
2021-10-26 11:26:51 +0200

    Created text similarity for given texts

--- src/app/components/text-similarity/text-similarity.component.ts
+++ src/app/components/text-similarity/text-similarity.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TextSimilarityService} from "../../services/text-similarity.service";
 
 @Component({
   selector: 'app-text-similarity',
@@ -9,13 +10,20 @@ export class TextSimilarityComponent implements OnInit {
 
   inputText1: string;
   inputText2: string;
+  similarity: number | undefined
 
-  constructor() {
+  constructor(private textSimilarityService: TextSimilarityService) {
     this.inputText1 = "";
     this.inputText2 = "";
   }
 
   ngOnInit(): void {
+  }
+
+  getTextSimilarity() {
+    this.textSimilarityService.getSentimentAnalysis(this.inputText1, this.inputText2).subscribe((response) => {
+      this.similarity = response.similarity
+    })
   }
 
 }
--- src/app/services/text-similarity.service.ts
+++ src/app/services/text-similarity.service.ts
@@ -1,5 +1,8 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {TextSimilarityResponse} from "../models/text-similarity/TextSimilarityResponse";
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +11,17 @@ export class TextSimilarityService {
 
   private readonly apiUrl = environment.postApi
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
   }
+
+  getSentimentAnalysis(inputText1: string, inputText2: string): Observable<TextSimilarityResponse> {
+    let params = new HttpParams();
+    params = params.append('text1', inputText1);
+    params = params.append('text2', inputText2);
+    // @ts-ignore
+    params = params.append('token', localStorage.getItem("token"));
+
+    return this.httpClient.get<TextSimilarityResponse>(`${this.apiUrl}sim/v1/?`, {params: params});
+  }
+
 }

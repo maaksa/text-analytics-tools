f44b1cd8abb3747c2b8c530c000b3b88424abcfa
Maksa <milosmilos@ymail.com>
2021-10-26 11:31:57 +0200

    Created language detection for given input

--- src/app/components/language-detection/language-detection.component.ts
+++ src/app/components/language-detection/language-detection.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {LanguageDetectionService} from "../../services/language-detection.service";
 
 @Component({
   selector: 'app-language-detection',
@@ -9,15 +10,25 @@ export class LanguageDetectionComponent implements OnInit {
 
   inputText: string;
   clean: boolean;
-  confidenceLevel: number;
+  confidenceLevel: number | undefined;
+  lang: string;
 
-  constructor() {
+  constructor(private languageDetectionService: LanguageDetectionService) {
     this.inputText = '';
     this.clean = false;
-    this.confidenceLevel = 0
+    this.lang = ''
   }
 
   ngOnInit(): void {
+  }
+
+  getLanguageDetection() {
+    this.languageDetectionService.getLanguageDetection(this.inputText, this.clean).subscribe((response) => {
+      response.detectedLangs.map(value => {
+        this.lang = value.lang
+        this.confidenceLevel = value.confidence
+      })
+    })
   }
 
 }
--- src/app/services/language-detection.service.ts
+++ src/app/services/language-detection.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {LanguageDetectionResponse} from "../models/language-detection/LanguageResponse";
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +11,20 @@ export class LanguageDetectionService {
 
   private readonly apiUrl = environment.postApi
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getLanguageDetection(inputText: string, clean: boolean): Observable<LanguageDetectionResponse> {
+    let params = new HttpParams();
+    if (clean) {
+      params = params.append('clean', clean)
+    }
+    params = params.append('text', inputText);
+    params = params.append('min_confidence', clean);
+    // @ts-ignore
+    params = params.append('token', localStorage.getItem("token"));
+
+    return this.httpClient.get<LanguageDetectionResponse>(`${this.apiUrl}li/v1/?`, {params: params});
+  }
+
 }
